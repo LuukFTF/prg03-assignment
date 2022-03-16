675b0eae647efe30d7a85c08fb5ee4155d2d6982
LucasvdVegt <lpsvandervegt@gmail.com>
2022-03-16 16:49:20 +0100

    add prg03 stuff

--- public/index.php
+++ public/index.php
@@ -1,4 +1,25 @@
-<h1>test</h1>
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lucas's Portfolio</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/index.js"></script>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="/">/home</a></li>
+                <li><a href="/cmgt">/cmgt</a></li>
+                <li><a href="/">/dev</a></li>
+                <li><a href="/">/video</a></li>
+            </ul>
+        </nav>
+        <section>
+            <img src="images/pf.jpg" height="200"></img>
+            <h1>Lucas van der Vegt</h1>
+        </section>
+  </body>
+</html>
