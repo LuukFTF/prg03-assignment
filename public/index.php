0bc9209de141da6780ca18017b0f2cb14e2ff7d5
LucasvdVegt <lpsvandervegt@gmail.com>
2022-03-17 12:33:15 +0100

    frontend html

--- public/index.php
+++ public/index.php
@@ -9,17 +9,44 @@
         <script src="js/index.js"></script>
     </head>
     <body>
-        <nav>
-            <ul>
-                <li><a href="/">/home</a></li>
-                <li><a href="/cmgt">/cmgt</a></li>
-                <li><a href="/">/dev</a></li>
-                <li><a href="/">/video</a></li>
-            </ul>
-        </nav>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/">/home</a></li>
+                    <li><a href="/cmgt">/cmgt</a></li>
+                    <li><a href="/">/dev</a></li>
+                    <li><a href="/">/video</a></li>
+                </ul>
+                <div>
+                    <form>
+                        
+                    </form>
+                    guest@lucas~$
+                </div>
+            </nav>
+        </header>
         <section>
             <img src="images/pf.jpg" height="200"></img>
             <h1>Lucas van der Vegt</h1>
+            <div>
+                <a>Webdeveloper</a>
+                <a>UX Designer</a>
+                <a>Gamification</a>
+                <a>Video Production</a>
+                <a>Social Media Management</a>
+                <a>Reflax</a> 
+            </div>
+        </section>
+        <section>
+            <div>
+                <img src="images/reflax-dekuip.jpg" height="200"></img>
+                <div>
+                    <h2>Project 1</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolore voluptate ullam molestias recusandae placeat nisi rerum ipsa. Consectetur, dolor.
+                    </p>
+                </div>
+            </div>
         </section>
   </body>
 </html>
