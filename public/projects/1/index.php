0171010b7d0487246dbea862954d2e10808ae3fd
LucasvdVegt <lpsvandervegt@gmail.com>
2022-03-21 17:15:10 +0100

    frontend & icons

--- public/projects/1/index.php
+++ public/projects/1/index.php
@@ -11,26 +11,24 @@
     <body>
         <header>
             <nav>
-                <div>                
+                <div class="links flex">                
                     <a class="logo" href="/"><img src="../../images/pf.jpg" height="40" width="40"></img>Lucas</a>
-                    <ul>
+                    <ul class="flex">
                         <li><a href="/">/home</a></li>
                         <li><a href="/cmgt">/cmgt</a></li>
                         <li><a href="/">/dev</a></li>
                         <li><a href="/">/video</a></li>
                     </ul>
-                    <a class="theme" href="/"><img src="../../images/pf.jpg" height="40" width="40"></img></a>
-                </div>
-                <div>
-                    <form class="cli">
-                        <label>guest@lucas /projects/1/ $</label>
-                        <input type="text"></input>
-                    </form>
-                </div>
+                    <a class="theme" href="/"><svg height="40" width="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M256 159.1c-53.02 0-95.1 42.98-95.1 95.1S202.1 351.1 256 351.1s95.1-42.98 95.1-95.1S309 159.1 256 159.1zM509.3 347L446.1 255.1l63.15-91.01c6.332-9.125 1.104-21.74-9.826-23.72l-109-19.7l-19.7-109c-1.975-10.93-14.59-16.16-23.72-9.824L256 65.89L164.1 2.736c-9.125-6.332-21.74-1.107-23.72 9.824L121.6 121.6L12.56 141.3C1.633 143.2-3.596 155.9 2.736 164.1L65.89 256l-63.15 91.01c-6.332 9.125-1.105 21.74 9.824 23.72l109 19.7l19.7 109c1.975 10.93 14.59 16.16 23.72 9.824L256 446.1l91.01 63.15c9.127 6.334 21.75 1.107 23.72-9.822l19.7-109l109-19.7C510.4 368.8 515.6 356.1 509.3 347zM256 383.1c-70.69 0-127.1-57.31-127.1-127.1c0-70.69 57.31-127.1 127.1-127.1s127.1 57.3 127.1 127.1C383.1 326.7 326.7 383.1 256 383.1z"/></svg></a>
+                </div>                
+                <form class="cli">
+                    <label>guest@lucas ~$</label>
+                    <input type="text"></input>
+                </form>
             </nav>
         </header>
         <div class="webcontainer">
-            <section class="project">
+            <article class="project">
                 <img src="../../images/reflax-dekuip.jpg" height="200" width="800"></img>
                 <div class="textheader">
                     <h1># Project 1</h1>
@@ -42,10 +40,10 @@
                 </div>
                 <div class="textbody">
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias excepturi est porro voluptas minima et officia consequuntur dicta, atque vitae voluptate, assumenda temporibus eligendi neque pariatur? Sint quae dicta voluptatum.</p>
-                    <img src="../../images/reflax-dekuip.jpg" height="200"></img>
+                    <img src="../../images/reflax-dekuip2.jpg" height="200"></img>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias excepturi est porro voluptas minima et officia consequuntur dicta, atque vitae voluptate, assumenda temporibus eligendi neque pariatur? Sint quae dicta voluptatum.</p>
                 </div>
-            </section>
+            </article>
         </div>
   </body>
 </html>
